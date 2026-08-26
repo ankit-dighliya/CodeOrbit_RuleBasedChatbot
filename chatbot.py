6036d8751e97e63c1240340717ea451778ebe6f0
Ankit Kumar <kumarankis120@gmail.com>
2026-08-26 21:54:48 +0530

    Add some conditions

--- chatbot.py
+++ chatbot.py
@@ -3,3 +3,18 @@ name = input("Bot: What is your name? ")
 
 print("You:", name)
 print(f"Bot: You said {name}")
+
+
+print("Bot: I am ready to chat!")
+
+message = ""
+
+while message != "bye":
+
+    message = input("You: ").strip().lower()
+    if message == "bye":
+        print("Goodbye! Have a great day!")
+    elif message in ["hi", "hello", "hey"]:
+        print("Bot: Hello! How can I help you?")
+    else:
+        print(f"Bot: You said {message}")
